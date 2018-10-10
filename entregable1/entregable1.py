b9c95f95067570e45e77a426c611c0355fe62ffb
Gema <al315684@uji.es>
2018-10-10 12:35:06 +0200

    Resolución del entregable 1, añadir puntos de encontrar camino único, encontrar arista y montar nuevo camino

--- entregable1/entregable1.py
+++ entregable1/entregable1.py
@@ -1,7 +1,11 @@
 from algoritmia.datastructures.mergefindsets import MergeFindSet
 from algoritmia.datastructures.digraphs import UndirectedGraph
-from Utils.labyrinthviewer import LabyrinthViewer
+from algoritmia.datastructures.queues import Fifo
+
+from Utiles.labyrinthviewer import LabyrinthViewer
 import sys
+
+
 
 
 def create_labyrinth(rows: int, cols: int, corridors) -> UndirectedGraph:
@@ -13,6 +17,39 @@ def create_labyrinth(rows: int, cols: int, corridors) -> UndirectedGraph:
     return UndirectedGraph(E=corridors)
 
 
+def matriz_saltos(grafo, v_inicial, rows, cols):
+    m = []
+    for i in range(rows):
+        m.append([0] * cols)
+    vertices = []
+    queue = Fifo()
+    seen = set()
+    queue.push((v_inicial, v_inicial))
+    while len(queue) > 0:
+        v = queue.pop()
+        vertices.append(v)
+        print(grafo.succs(v))
+        for suc in grafo.succs(v):
+            if suc not in seen:
+                seen.add(suc)
+                m[suc[0][suc[1]]] = m[v[0]][v[1]] + 1
+                queue.push(suc)
+    return m, vertices
+
+
+def encuentra_muro(mat_inicio, mat_final, descartados):
+    min = mat_final[0][0]
+    muro = set()
+
+    for s, t in range(descartados):
+        suma = mat_inicio[s[0]][s[1]] + mat_final[t[0]][t[1]]
+        if suma < min:
+            min = suma
+            muro = s, t
+
+    return muro, min
+
+
 if __name__ == '__main__':
     f = open(sys.argv[1])
     fichero = []
@@ -21,8 +58,9 @@ if __name__ == '__main__':
         fichero.append(linea.split(','))
 
     f.close()
+
     edges = []
-    descartados=[]
+    descartados = []
     for i in range(len(fichero)):
         for j in range(len(fichero[0])):
             if 's' not in fichero[i][j]:
@@ -35,11 +73,18 @@ if __name__ == '__main__':
                 descartados.append(((i, j), (i, j + 1)))
     rows = len(fichero)
     cols = len(fichero[0])
+
+    graph = create_labyrinth(rows, cols, edges)
+    source = 0,0
+    target = (rows - 1, cols -1)
+    print(graph)
+    mat_inicio = matriz_saltos(graph, source, rows, cols)
+    mat_final = matriz_saltos(graph, target, rows, cols)
+
+    muro = encuentra_muro(mat_inicio, mat_final, descartados)
+    edges.append(muro)
     print(edges)
     print(descartados)
 
-    graph = create_labyrinth(rows, cols,edges)
     viewer = LabyrinthViewer(graph, canvas_width=800, canvas_height=480, margin=10)
     viewer.run()
-
-
