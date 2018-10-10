1c9ddc236ac0b9928b6fad660fc42b6b6d2ed9df
Abraham Benitez Sabao <al292072@uji.es>
2018-10-10 12:57:46 +0200

    Hay un error a la hora de encontar el camino más corto.

--- entregable1/entregable1.py
+++ entregable1/entregable1.py
@@ -1,12 +1,9 @@
 from algoritmia.datastructures.mergefindsets import MergeFindSet
 from algoritmia.datastructures.digraphs import UndirectedGraph
 from algoritmia.datastructures.queues import Fifo
-
-from Utiles.labyrinthviewer import LabyrinthViewer
 import sys
 
-
-
+from Utils.labyrinthviewer import LabyrinthViewer
 
 def create_labyrinth(rows: int, cols: int, corridors) -> UndirectedGraph:
     vertices = [(r, c) for r in range(rows) for c in range(cols)]
@@ -16,6 +13,34 @@ def create_labyrinth(rows: int, cols: int, corridors) -> UndirectedGraph:
 
     return UndirectedGraph(E=corridors)
 
+def path(g, source: "T", target: "T"):
+    la = traveler_edges_width(g, source)
+    bp = {}
+    for (u, v) in la:
+        bp[v] = u
+    road = []
+    v = target
+    road.append(v)
+    while bp[v] != v:
+        v = bp[v]
+        road.append(v)
+
+    return road
+
+def traveler_edges_width(graph, v_initial):
+    edges = []
+    queue = Fifo()
+    seen = set()
+    queue.push((v_initial, v_initial))
+    seen.add(v_initial)
+    while len(queue) > 0:
+        u, v = queue.pop()
+        edges.append((u, v))
+        for suc in graph.succs(v):
+            if suc not in seen:
+                seen.add(suc)
+                queue.push((v, suc))
+    return edges
 
 def matriz_saltos(grafo, v_inicial, rows, cols):
     m = []
@@ -26,14 +51,14 @@ def matriz_saltos(grafo, v_inicial, rows, cols):
     seen = set()
     queue.push((v_inicial, v_inicial))
     while len(queue) > 0:
-        v = queue.pop()
-        vertices.append(v)
-        print(grafo.succs(v))
+        u, v = queue.pop()
+        vertices.append((u, v))
+        print("v",v,"u:",u)
         for suc in grafo.succs(v):
             if suc not in seen:
                 seen.add(suc)
-                m[suc[0][suc[1]]] = m[v[0]][v[1]] + 1
-                queue.push(suc)
+                m[suc[0]][suc[1]] = m[v[0]][v[1]] + 1
+                queue.push((v, suc))
     return m, vertices
 
 
@@ -41,7 +66,7 @@ def encuentra_muro(mat_inicio, mat_final, descartados):
     min = mat_final[0][0]
     muro = set()
 
-    for s, t in range(descartados):
+    for s, t in descartados:
         suma = mat_inicio[s[0]][s[1]] + mat_final[t[0]][t[1]]
         if suma < min:
             min = suma
@@ -66,25 +91,37 @@ if __name__ == '__main__':
             if 's' not in fichero[i][j]:
                 edges.append(((i, j), (i + 1, j)))
             else:
-                descartados.append(((i, j), (i + 1, j)))
+                if i + 1 < len(fichero):
+                    descartados.append(((i, j), (i + 1, j)))
             if 'e' not in fichero[i][j]:
                 edges.append(((i, j), (i, j + 1)))
             else:
-                descartados.append(((i, j), (i, j + 1)))
+                if j + 1 < len(fichero[0]):
+                    descartados.append(((i, j), (i, j + 1)))
+
     rows = len(fichero)
     cols = len(fichero[0])
 
     graph = create_labyrinth(rows, cols, edges)
-    source = 0,0
-    target = (rows - 1, cols -1)
+    source = 0, 0
+    target = rows - 1, cols -1
+
     print(graph)
-    mat_inicio = matriz_saltos(graph, source, rows, cols)
-    mat_final = matriz_saltos(graph, target, rows, cols)
+    mat_inicio, la = matriz_saltos(graph, source, rows, cols)
+    mat_final, miau= matriz_saltos(graph, target, rows, cols)
 
     muro = encuentra_muro(mat_inicio, mat_final, descartados)
     edges.append(muro)
-    print(edges)
-    print(descartados)
+    new_graph = create_labyrinth(rows, cols, edges)
+
+
+    road = path(graph, source, target)
+    new_road = path(new_graph, source, target)
+
+    print(len(road))
+    print(len(new_road))
 
     viewer = LabyrinthViewer(graph, canvas_width=800, canvas_height=480, margin=10)
+    viewer.add_path(new_road, color="green", offset=-3)
+    viewer.add_path(road, color="blue")
     viewer.run()
