7837398c3d84279cc5acd479a808d1b11eb11dc2
Abraham Benitez Sabao <al292072@uji.es>
2018-10-15 19:06:57 +0200

    Comentarios del código.

--- entregable1/entregable1.py
+++ entregable1/entregable1.py
@@ -111,24 +111,42 @@ if __name__ == '__main__':
     cols = len(fichero[0])
     source = (0, 0)
     target = (rows - 1, cols - 1)
+
+    # Paso 2: Obtener la lista de aristas del laberinto y las aristas descartadas
     edges, descartados = get_edges(fichero)
 
+    # Paso 3: Crear el grafo para recorrer el único camino
     graph = create_labyrinth(rows, cols, edges)
+
+    # Paso 4: Crear las matrices de saltos desde el inico y desde el final del laberinto
     matrix_inicio = jump_matrix(graph, rows, cols, (0, 0))
     matrix_fin = jump_matrix(graph, rows, cols, (rows - 1, cols - 1))
 
-    edge, distance = get_remove_edge(matrix_inicio, matrix_fin, descartados)
-    edges.append(edge)
-    new_graph = create_labyrinth(rows, cols, edges)
+    # Paso 5: Obtener el vértice que hay que añadir al grafo para encontrar el camino más corto y la distancia
+    edge, new_distance = get_remove_edge(matrix_inicio, matrix_fin, descartados)
 
-    road = path(graph, source, target)
-    new_road = path(new_graph, source, target)
-
+    #Paso 6: Mostrar el resultado por pantalla para pasar los tests
     print(' '.join([str(i[0]) + " " + str(i[1]) for i in edge]))
-    print(len(road)-1)
-    print(distance+1)
 
-    #print("\n\n--- %s seconds ---" % (time.time() - start_time))
+    # Indistintamente puede usarse una forma o la otra, tener en cuenta las carácterísticas de estas matrices
+    print(matrix_fin[0][0])
+    #print(matrix_inicio[rows-1][cols-1])
+
+    print(new_distance+1)
+
+    ''' No es necesario añadir el vertice en el grafo ni volver a calcular el camino,
+        porque podemos obtenerlos sin necesidad de calcular nuevos caminos, como
+        se hace anteriormente. Los siguientes pasos se han de realizar en el caso
+        que se quiera mostrar ambos caminos por cada laberinto, además del 
+        tiempo que ha tardado en realizar los cálculos
+    '''
+    # edges.append(edge)
+    # new_graph = create_labyrinth(rows, cols, edges)
+    #
+    # road = path(graph, source, target)
+    # new_road = path(new_graph, source, target)
+
+    # print("\n\n--- %s seconds ---" % (time.time() - start_time))
 
     # viewer = LabyrinthViewer(graph, canvas_width=800, canvas_height=480, margin=10)
     # viewer.add_path(new_road, color="green", offset=-3)
