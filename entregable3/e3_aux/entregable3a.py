fa90a986ad71c113f093205d76c8c71fcaa475ed
Abraham Benitez Sabao <al292072@uji.es>
2018-11-13 12:53:00 +0100

    Terminado entregable3, falta comprobar su funcionamiento.

--- entregable3/e3_aux/entregable3a.py
+++ entregable3/e3_aux/entregable3a.py
@@ -8,24 +8,23 @@ from Utils.bt_scheme import PartialSolutionWithVisitedControl, Solution, State
 def bricker_vc_solve(level: Level):
     class BrikerVC_PS(PartialSolutionWithVisitedControl):
         def __init__(self, block: Block, decisions: Tuple[Move, ...]):
-            # TODO: Implementar
-            raise NotImplementedError
+            self.block = block
+            self.decisions=decisions
+            self.n = len(decisions)
 
         def is_solution(self) -> bool:
-            # TODO: Implementar
-            raise NotImplementedError
+            return self.block.is_standing_at_pos(level.get_targetpos())
 
         def get_solution(self) -> Solution:
-            # TODO: Implementar
-            raise NotImplementedError
+            return self.decisions
 
         def successors(self) -> Iterable["BrikerVC_PS"]:
-            # TODO: Implementar
-            raise NotImplementedError
+            if not self.is_solution():
+                for elem in self.block.valid_moves(level.is_valid):
+                    yield BrikerVC_PS(self.block.move(elem), self.decisions+(elem,))
 
         def state(self) -> State:
-            # TODO: Implementar
-            raise NotImplementedError
+            return self.block
 
     # TODO: crea initial_ps y llama a BacktrackingVCSolver.solve
     raise NotImplementedError
